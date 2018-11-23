02060764f0bbee16b3c77aa2b3b1d8fe22256cbc
zakikakar <36473212+zakikakar@users.noreply.github.com>
2018-11-23 13:31:04 +0100

    broadcast zaki number

---
+++ zakiUDPNumberBroadcast.py
@@ -0,0 +1,37 @@
+from sense_hat import SenseHat
+from time import sleep
+import socket
+
+sense = SenseHat()
+
+def setup_udp_socket():
+    # Setup UDP socket for broadcasting
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    # Set a timeout so the socket does not block
+    # indefinitely when trying to receive data.
+    server.settimeout(0.2)
+
+    #server.bind(("", 44444))
+    return server
+
+sense.clear()
+
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+yellow = (255,255,0)
+nothing = (0,0,0)
+number = 1
+
+s = setup_udp_socket()
+
+while True:
+    # Broadcast message to port 64545 via UDP Socket
+    s.sendto(str(number).encode(), ('<broadcast>', 64545))
+
+    sense.show_message(str(number), scroll_speed=0.1, text_colour=yellow, back_colour=blue)
+
+    number+=1
+    sleep(0.5)
